19d9153af49fe321ec51ba265dfae4634cf91484
JerryBeGood <mdobrowolski.1997@gmail.com>
2021-10-10 14:01:39 +0200

    CRYPTO-107 Mock binanceAccountInfo query resolver with dummy data

--- cryptofolio/graphql/binance/binance_resolvers.py
+++ cryptofolio/graphql/binance/binance_resolvers.py
@@ -10,34 +10,55 @@ def resolve_binanceAccountInfo(obj, info, API_key, secret, recvWindow=5000):
 
     payload = {}
 
-    timestamp = int(round(time.time() * 1000))
-    request_body = f'recvWindow={recvWindow}&timestamp={timestamp}'
-    signature = hmac.new(secret.encode(),
-                         request_body.encode('UTF-8'),
-                         digestmod=hashlib.sha256).hexdigest()
+    # timestamp = int(round(time.time() * 1000))
+    # request_body = f'recvWindow={recvWindow}&timestamp={timestamp}'
+    # signature = hmac.new(secret.encode(),
+    #                      request_body.encode('UTF-8'),
+    #                      digestmod=hashlib.sha256).hexdigest()
 
-    with requests.get(f'https://testnet.binance.vision/api/v3/account',
-                      params={
-                          'recvWindow': recvWindow,
-                          'timestamp': timestamp,
-                          'signature': signature
-                      },
-                      headers={'X-MBX-APIKEY': API_key}) as response:
+    # with requests.get(f'https://testnet.binance.vision/api/v3/account',
+    #                   params={
+    #                       'recvWindow': recvWindow,
+    #                       'timestamp': timestamp,
+    #                       'signature': signature
+    #                   },
+    #                   headers={'X-MBX-APIKEY': API_key}) as response:
 
-        response_json = response.json()
+    #     response_json = response.json()
 
-        payload['succes'] = True
-        payload['errors'] = ""
-        payload['accountType'] = response_json['accountType']
-        payload['balances'] = []
+    # payload['succes'] = True
+    # payload['errors'] = ""
+    # payload['accountType'] = response_json['accountType']
+    # payload['balances'] = []
 
-        for asset in response_json['balances']:
-            balance = {}
-            balance['asset'] = asset['asset']
-            balance['free'] = asset['free']
-            balance['locked'] = asset['locked']
+    # for asset in response_json['balances']:
+    #     balance = {}
+    #     balance['asset'] = asset['asset']
+    #     balance['free'] = asset['free']
+    #     balance['locked'] = asset['locked']
 
-            payload['balances'].append(balance)
+    #     payload['balances'].append(balance)
+
+    balances = [{
+        'asset': 'BNB',
+        'percentage': 40.0,
+        'free': 1000.0,
+        'locked': 0.0
+    }, {
+        'asset': 'BTC',
+        'percentage': 40.0,
+        'free': 1000.0,
+        'locked': 0
+    }, {
+        'asset': 'ETH',
+        'percentage': 25.0,
+        'free': 500.0,
+        'locked': 0.0
+    }]
+
+    payload['totalValue'] = 2500.0
+    payload['valueChangePercentage'] = 5.0
+    payload['balances'] = balances
 
     return payload
 
