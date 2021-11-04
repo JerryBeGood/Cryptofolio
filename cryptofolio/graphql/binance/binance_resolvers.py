195092c03bbdfb904b7189b82c2e0ca64eb9dfdf
JerryBeGood <mdobrowolski.1997@gmail.com>
2021-11-04 20:29:59 +0100

    CRYPTO-116 Implement binanceAccountInfo resolver

--- cryptofolio/graphql/binance/binance_resolvers.py
+++ cryptofolio/graphql/binance/binance_resolvers.py
@@ -2,68 +2,51 @@ import requests
 import time
 import hmac, hashlib
 
-from cryptofolio.utility import EXCHANGE_INFO
+from cryptofolio.utility import EXCHANGE_INFO, ASSET_TICKER_INFO
 
 
-# binanceAccountInfo query resolver
 def resolve_binanceAccountInfo(obj, info, API_key, secret, recvWindow=5000):
-
     payload = {}
+    payload['totalValue'] = 0.0
+    payload['valueChangePercentage'] = 0.0
+    payload['balances'] = []
 
-    # timestamp = int(round(time.time() * 1000))
-    # request_body = f'recvWindow={recvWindow}&timestamp={timestamp}'
-    # signature = hmac.new(secret.encode(),
-    #                      request_body.encode('UTF-8'),
-    #                      digestmod=hashlib.sha256).hexdigest()
+    response_json = binance_account_info_request(API_key, secret, recvWindow)
 
-    # with requests.get(f'https://testnet.binance.vision/api/v3/account',
-    #                   params={
-    #                       'recvWindow': recvWindow,
-    #                       'timestamp': timestamp,
-    #                       'signature': signature
-    #                   },
-    #                   headers={'X-MBX-APIKEY': API_key}) as response:
+    for asset in response_json['balances']:
+        balance = {}
+        balance['asset'] = asset['asset']
 
-    #     response_json = response.json()
+        if asset['asset'] in ['USDT', 'BUSD']:
+            balance['percentage'] = float(asset['free'])
+            payload['totalValue'] += balance['percentage']
+        else:
+            if f'{asset["asset"]}USDT' in ASSET_TICKER_INFO.keys():
+                balance['percentage'] = float(
+                    ASSET_TICKER_INFO[f'{asset["asset"]}USDT']
+                    ['price']) * float(asset['free'])
+            else:
+                balance['percentage'] = None
 
-    # payload['succes'] = True
-    # payload['errors'] = ""
-    # payload['accountType'] = response_json['accountType']
-    # payload['balances'] = []
+            payload['totalValue'] += balance['percentage']
 
-    # for asset in response_json['balances']:
-    #     balance = {}
-    #     balance['asset'] = asset['asset']
-    #     balance['free'] = asset['free']
-    #     balance['locked'] = asset['locked']
+        payload['balances'].append(balance)
 
-    #     payload['balances'].append(balance)
+    for asset in payload['balances']:
+        if asset['asset'] not in ['USDT', 'BUSD']:
+            payload['valueChangePercentage'] += float(
+                ASSET_TICKER_INFO[f'{asset["asset"]}USDT']
+                ['priceChangePercent']) * (asset['percentage'] / 100)
+        asset['percentage'] = round(
+            asset['percentage'] / (payload['totalValue'] / 100), 2)
 
-    balances = [{
-        'asset': 'BNB',
-        'percentage': 40.0,
-        'free': 1000.0,
-        'locked': 0.0
-    }, {
-        'asset': 'BTC',
-        'percentage': 40.0,
-        'free': 1000.0,
-        'locked': 0
-    }, {
-        'asset': 'ETH',
-        'percentage': 25.0,
-        'free': 500.0,
-        'locked': 0.0
-    }]
-
-    payload['totalValue'] = 2500.0
-    payload['valueChangePercentage'] = 5.0
-    payload['balances'] = balances
+    payload['totalValue'] = round(payload['totalValue'], 2)
+    payload['valueChangePercentage'] = round(
+        payload['valueChangePercentage'] / (payload['totalValue'] / 100), 2)
 
     return payload
 
 
-# binanceExchangeInfo query resolver
 def resolve_binanceExchangeInfo(obj, info, symbols=None):
 
     keys = EXCHANGE_INFO.keys()
@@ -79,7 +62,6 @@ def resolve_binanceExchangeInfo(obj, info, symbols=None):
     return payload
 
 
-# binanceSPOTStopLossLimitOrder mutation resolver
 def resolve_binanceSPOTStopLossLimitOrder(obj, info, API_key, secret, order):
     payload = {}
     params = {}
@@ -136,7 +118,6 @@ def resolve_binanceSPOTStopLossLimitOrder(obj, info, API_key, secret, order):
     return payload
 
 
-# binanceSPOTMarketOrder mutation resolver
 def resolve_binanceSPOTMarketOrder(obj, info, API_key, secret, order):
 
     payload = {}
@@ -185,7 +166,6 @@ def resolve_binanceSPOTMarketOrder(obj, info, API_key, secret, order):
     return payload
 
 
-# binanceSPOTLimitOrder mutation resolver
 def resolve_binanceSPOTLimiOrder(info, obj, API_key, secret, order):
 
     payload = {}
@@ -237,3 +217,21 @@ def resolve_binanceSPOTLimiOrder(info, obj, API_key, secret, order):
             payload['status'] = response_json['status']
 
     return payload
+
+
+def binance_account_info_request(API_key, secret, recvWindow):
+    timestamp = int(round(time.time() * 1000))
+    request_body = f'recvWindow={recvWindow}&timestamp={timestamp}'
+    signature = hmac.new(secret.encode(),
+                         request_body.encode('UTF-8'),
+                         digestmod=hashlib.sha256).hexdigest()
+
+    with requests.get(f'https://testnet.binance.vision/api/v3/account',
+                      params={
+                          'recvWindow': recvWindow,
+                          'timestamp': timestamp,
+                          'signature': signature
+                      },
+                      headers={'X-MBX-APIKEY': API_key}) as response:
+
+        return response.json()
