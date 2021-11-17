e27d397aeaec7ab18712acd64cdac4be8dfd176f
JerryBeGood <mdobrowolski.1997@gmail.com>
2021-11-17 19:21:53 +0100

    CRYPTO-128 Add User database model

--- cryptofolio/models.py
+++ cryptofolio/models.py
@@ -8,3 +8,11 @@ class BaseModel(db.Model):
 
     def __init__(self, *args):
         super().__init__(*args)
+
+
+class User(BaseModel):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), unique=True, nullable=False)
+    is_activated = db.Column(db.Boolean, nullable=False)
+    activation_code = db.Column(db.String(8), nullable=False)
