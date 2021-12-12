32f4ad42d9bc5b138fd8946c07fee6b374d3a14d
JerryBeGood <mdobrowolski.1997@gmail.com>
2021-12-12 14:07:49 +0100

    CRYPTO-138 Connect change_password_resolver to the schema"

--- cryptofolio/graphql_api/schema/user_account/__init__.py
+++ cryptofolio/graphql_api/schema/user_account/__init__.py
@@ -24,4 +24,7 @@ user_account_mutation.set_field('generatePswdRecoveryCode',
 user_account_mutation.set_field('recoverPassword',
                                 user_account.recover_password_resolver)
 user_account_mutation.set_field('deleteAccount',
-                                user_account.delete_account_resolver)             
+                                user_account.delete_account_resolver)        
+user_account_mutation.set_field('changePassword',
+                                user_account.change_password_resolver)
+                                
