66585a50fd251a0411f22fee1e4d35a69fe95c96
JerryBeGood <mdobrowolski.1997@gmail.com>
2021-12-14 18:12:33 +0100

    CRYPTO-135 Add validate_token and fetch_exchange_credetnials functions

--- cryptofolio/utilities.py
+++ cryptofolio/utilities.py
@@ -1,4 +1,31 @@
 import requests
+import jwt
+
+from cryptography.fernet import Fernet
+
+from cryptofolio import app
+from cryptofolio.models import Exchange
+
+
+def fetch_exchange_credentials(token_claims, exchange):
+
+    exchange_credentials = Exchange.query.filter_by(
+        user_id=token_claims['iss']).filter_by(exchange=exchange).first()
+    print(exchange_credentials)
+    if not exchange_credentials:
+        return False, f"{exchange} credentials doesn't exist for this account"
+
+    try:
+        cipher_suite = Fernet(app.config.get('EXCHANGE_SECRET_KEY'))
+        API_key = cipher_suite.decrypt(
+            str.encode(exchange_credentials.api_key)).decode('utf-8')
+        secret = cipher_suite.decrypt(
+            str.encode(exchange_credentials.secret)).decode('utf-8')
+    except Exception as error:
+        print(str(error))
+        return False, 'Decryption error'
+    else:
+        return True, API_key, secret
 
 
 def exchange_info():
@@ -34,6 +61,37 @@ def asset_ticker_info():
             }
 
     return payload
+
+
+def validate_token(authToken):
+    try:
+        jwt_claims = jwt.decode(
+            jwt=authToken,
+            key=app.config.get('SECRET_KEY'),
+            algorithms=['HS256'],
+            options={
+                'verify_signature': True,
+                'require': ['exp', 'iat', 'iss'],
+                'verify_exp': True,
+                'verify_iat': True
+            }
+        )
+    except jwt.exceptions.InvalidTokenError as error:
+        return False, str(error)
+    except jwt.exceptions.DecodeError as error:
+        return False, str(error)
+    except jwt.exceptions.ExpiredSignatureError as error:
+        return False, str(error)
+    except jwt.exceptions.InvalidIssuedAtError as error:
+        return False, str(error)
+    except jwt.exceptions.InvalidKeyError as error:
+        return False, str(error)
+    except jwt.exceptions.InvalidAlgorithmError as error:
+        return False, str(error)
+    except jwt.exceptions.MissingRequiredClaimError as error:
+        return False, str(error)
+    else:
+        return True, jwt_claims
 
 
 EXCHANGE_INFO = exchange_info()
