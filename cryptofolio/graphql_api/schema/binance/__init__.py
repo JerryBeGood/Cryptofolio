098406e70c0307d7c1830ba4dd72a681adede85a
JerryBeGood <mdobrowolski.1997@gmail.com>
2021-12-31 18:04:02 +0100

    CRYPTO-165 Adjust binance schema

--- cryptofolio/graphql_api/schema/binance/__init__.py
+++ cryptofolio/graphql_api/schema/binance/__init__.py
@@ -6,16 +6,8 @@ binance_type_defs = load_schema_from_path(
     'cryptofolio/graphql_api/schema/binance')
 
 binance_query = ObjectType("Query")
-binance_mutation = ObjectType("Mutation")
 
 binance_query.set_field('binanceAccountInfo',
                         binance.binance_account_info_resolver)
 binance_query.set_field('binanceExchangeInfo',
                         binance.binance_exchange_info_resolver)
-
-binance_mutation.set_field('binanceSPOTMarketOrder',
-                           binance.binance_spot_market_order_resolver)
-binance_mutation.set_field('binanceSPOTLimitOrder',
-                           binance.binance_spot_limit_order_resolver)
-binance_mutation.set_field('binanceSPOTStopLossLimitOrder',
-                           binance.binance_spot_stop_loss_limit_order_resolver)
