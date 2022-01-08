ae5aadd83f7d081cd5fcb2ae0ea1b9b329da4b0f
JerryBeGood <mdobrowolski.1997@gmail.com>
2022-01-08 16:17:59 +0100

    CRYPTO-176 Delete binance account_info query and AccountInformation type from schema

--- cryptofolio/graphql_api/schema/__init__.py
+++ cryptofolio/graphql_api/schema/__init__.py
@@ -1,5 +1,4 @@
 from ariadne import make_executable_schema
-from cryptofolio.graphql_api.schema.binance import binance_query, binance_type_defs
 from cryptofolio.graphql_api.schema.coingeko import coingeko_typ_defs, coingeko_query
 from cryptofolio.graphql_api.schema.user_account import user_account_type_defs, user_account_mutation
 from cryptofolio.graphql_api.schema.exchange import exchange_type_defs, exchange_mutation, exchange_query
@@ -7,10 +6,8 @@ from cryptofolio.graphql_api.schema.exchange import exchange_type_defs, exchange
 # Initialization of the shcema
 schema = make_executable_schema(
     [coingeko_typ_defs,
-     binance_type_defs,
      user_account_type_defs,
      exchange_type_defs],
     [coingeko_query,
-     binance_query,
      user_account_mutation,
      exchange_mutation, exchange_query])
