a4b00fa49bda5eae1c98046672ae7823db5c9d26
JerryBeGood <mdobrowolski.1997@gmail.com>
2022-01-08 13:22:34 +0100

    CRYPTO-175 Add bybit_spot_limit_order implementation

--- cryptofolio/graphql_api/resolvers/bybit/orders.py
+++ cryptofolio/graphql_api/resolvers/bybit/orders.py
@@ -5,10 +5,35 @@ from .utility import make_order, make_order_signature
 
 
 def bybit_spot_limit_order(authToken, order):
-    return {
-        'success': True,
-        'status': 'FILLED'
+
+    # Validate token
+    token_validation_payload = validate_token(authToken)
+    if not token_validation_payload[0]:
+        return {'success': token_validation_payload[0], 'msg': token_validation_payload[1]}
+
+    # Fetch exchange credentials
+    exchange_credentials = fetch_exchange_credentials(
+        token_validation_payload[1], 'bybit')
+    if not exchange_credentials[0]:
+        return {'success': False, 'msg': exchange_credentials[1]}
+
+    # Prepare request body
+    timestamp = int(round(time.time() * 1000))
+    params = {
+        'api_key': exchange_credentials[1],
+        'side': order["side"],
+        'symbol': order['symbol'],
+        'timestamp': timestamp,
+        'type': 'LIMIT',
+        'qty': order['quantity'],
+        'timeInForce': order['timeInForce'] if 'timeInForce' in order.keys() else 'GTC',
+        'price': order['price']
     }
+
+    params['sign'] = make_order_signature(params, exchange_credentials[2])
+    payload = make_order(params)
+
+    return payload
 
 
 def bybit_spot_market_order(authToken, order):
