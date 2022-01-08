f322076f48098323ffc226d51807ba71bd0c3d0a
JerryBeGood <mdobrowolski.1997@gmail.com>
2022-01-08 16:20:12 +0100

    CRYPTO-176 Add account_info_resolver

--- cryptofolio/graphql_api/resolvers/exchange/utility_resolvers.py
+++ cryptofolio/graphql_api/resolvers/exchange/utility_resolvers.py
@@ -2,8 +2,14 @@ from cryptofolio.graphql_api.resolvers.binance import binance_exchange_info
 from cryptofolio.graphql_api.resolvers.bybit import bybit_exchange_info
 
 
-def exchange_info_resolver(obj, info, exchange, symbols=None):
+def account_info_resolver(obj, info, exchange, authToken):
+    if exchange == 'binance':
+        return binance_account_info(authToken)
+    elif exchange == 'bybit':
+        return bybit_account_info(authToken)
 
+
+def exchange_info_resolver(obj, info, exchange, symbols=None):
     if exchange == 'binance':
         return binance_exchange_info(symbols)
     elif exchange == 'bybit':
