5ef25abcd07fe1c4ad6da83b3187ed0a8ea4eef1
JerryBeGood <mdobrowolski.1997@gmail.com>
2022-01-11 18:29:09 +0100

    CRYPTO-81 Move token and exchange_credentials validations from seperate order functions to orders resolvers

--- cryptofolio/graphql_api/resolvers/binance/orders.py
+++ cryptofolio/graphql_api/resolvers/binance/orders.py
@@ -10,23 +10,12 @@ from .utility import prepare_spot_market_limit_order_params, prepare_spot_market
 from cryptofolio.graphql_api.resolvers.shared_utilities import validate_token, fetch_exchange_credentials
 
 
-def binance_spot_stop_loss_limit_order(authToken, order):
+def binance_spot_stop_loss_limit_order(exchange_credentials, order):
     # Prepare request body
     payload = {}
     timestamp = int(round(time.time() * 1000))
     params = prepare_stop_loss_order_params(order, timestamp)
     request_body = prepare_stop_loss_order_request_body(order, timestamp)
-
-    # Validate token
-    token_validation_payload = validate_token(authToken)
-    if not token_validation_payload[0]:
-        return {'success': token_validation_payload[0], 'msg': token_validation_payload[1]}
-
-    # Fetch exchange credentials
-    exchange_credentials = fetch_exchange_credentials(
-        token_validation_payload[1], 'binance')
-    if not exchange_credentials[0]:
-        return {'success': False, 'msg': exchange_credentials[1]}
 
     # Preapre signature
     signature = hmac.new(exchange_credentials[2].encode(),
@@ -40,7 +29,7 @@ def binance_spot_stop_loss_limit_order(authToken, order):
     return payload
 
 
-def binance_spot_market_order(authToken, order):
+def binance_spot_market_order(exchange_credentials, order):
 
     payload = {}
 
@@ -48,17 +37,6 @@ def binance_spot_market_order(authToken, order):
     timestamp = int(round(time.time() * 1000))
     params = prepare_spot_market_order_params(order, timestamp)
     request_body = prepare_spot_market_order_request_body(order, timestamp)
-
-    # Validate token
-    token_validation_payload = validate_token(authToken)
-    if not token_validation_payload[0]:
-        return {'success': token_validation_payload[0], 'msg': token_validation_payload[1]}
-
-    # Fetch exchange credentials
-    exchange_credentials = fetch_exchange_credentials(
-        token_validation_payload[1], 'binance')
-    if not exchange_credentials[0]:
-        return {'success': False, 'msg': exchange_credentials[1]}
 
     signature = hmac.new(exchange_credentials[2].encode(),
                          request_body.encode('UTF-8'),
@@ -71,7 +49,7 @@ def binance_spot_market_order(authToken, order):
     return payload
 
 
-def binance_spot_limit_order(authToken, order):
+def binance_spot_limit_order(exchange_credentials, order):
 
     payload = {}
     # Prepare request data
@@ -79,17 +57,6 @@ def binance_spot_limit_order(authToken, order):
     params = prepare_spot_market_limit_order_params(order, timestamp)
     request_body = prepare_spot_market_limit_order_request_body(
         order, timestamp)
-
-    # Validate token
-    token_validation_payload = validate_token(authToken)
-    if not token_validation_payload[0]:
-        return {'success': token_validation_payload[0], 'msg': token_validation_payload[1]}
-
-    # Fetch exchange credentials
-    exchange_credentials = fetch_exchange_credentials(
-        token_validation_payload[1], 'binance')
-    if not exchange_credentials[0]:
-        return {'success': False, 'msg': exchange_credentials[1]}
 
     signature = hmac.new(exchange_credentials[2].encode(),
                          request_body.encode('UTF-8'),
