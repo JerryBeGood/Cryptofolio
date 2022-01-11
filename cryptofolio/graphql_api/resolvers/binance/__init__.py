3eddedae2158bc714ec9261e633b947be67436da
JerryBeGood <mdobrowolski.1997@gmail.com>
2022-01-11 11:51:15 +0100

    CRYPTO-177 Add mock of open_orders_resolver

--- cryptofolio/graphql_api/resolvers/binance/__init__.py
+++ cryptofolio/graphql_api/resolvers/binance/__init__.py
@@ -1,2 +1,2 @@
 from .orders import binance_spot_stop_loss_limit_order, binance_spot_limit_order, binance_spot_market_order
-from .utility import validate_binance_credentials, binance_exchange_info, binance_account_info
+from .utility import validate_binance_credentials, binance_exchange_info, binance_account_info, binance_open_orders
