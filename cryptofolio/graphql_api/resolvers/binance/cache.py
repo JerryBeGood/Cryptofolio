7e7f3d6f64adbf2fe4428fd6a203f77a6101d7ef
JerryBeGood <mdobrowolski.1997@gmail.com>
2022-01-18 17:06:10 +0100

    CRYPTO-185 Add update_binance_order_info

--- cryptofolio/graphql_api/resolvers/binance/cache.py
+++ cryptofolio/graphql_api/resolvers/binance/cache.py
@@ -4,3 +4,8 @@ from cryptofolio.graphql_api.resolvers.shared_utilities import prepare_binance_e
 BINANCE_EXCHANGE_INFO = prepare_binance_exchange_info()
 BINANCE_ASSET_TICKER_INFO = prepare_binance_asset_ticker_info()
 BINANCE_ORDERS_INFO = []
+
+
+def update_binance_order_info(symbol):
+    if symbol not in BINANCE_ORDERS_INFO:
+        BINANCE_ORDERS_INFO.append(symbol)
