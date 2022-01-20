2eb5aec64f4cbf06eb5d0538dfbe4c1249962365
JerryBeGood <mdobrowolski.1997@gmail.com>
2022-01-20 11:19:30 +0100

    CRYPTO-189 Add app_factory function: create_app

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ import sys
 
 from cryptofolio import app
 
-if __name__ == "__main__":
-    if '--test' in sys.argv:
+
+def create_app(config_file, app_mode):
+    app.config.from_pyfile(config_file)
+
+    if app_mode == '--real':
+        app.config['BINANCE'] = 'https://api.binance.com'
+        app.config['BYBIT'] = 'https://api.bybit.com'
+    else:
         app.config['BINANCE'] = 'https://testnet.binance.vision'
         app.config['BYBIT'] = 'https://api-testnet.bybit.com'
 
-    app.run(debug=True)
+    return app
+
+
+if __name__ == "__main__":
+    create_app(*sys.argv[1:]).run()
